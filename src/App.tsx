a40e7c3254b93e0a41842c9f408384d9f64e4f32
minsuk <alstjr0183@naver.com>
2022-09-26 19:17:26 +0900

    feat: globalStyle 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import GlobalStyle from "./styles/globalStyle";
+
 import Companys from "./pages/home/Companys";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Router>
         <Routes>
           <Route path="/" element={<Companys />} />
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  html{
+    font-size: 16px;
+    font-family: 'Noto Sans KR', sans-serif;     
+  };
+  
+  body {  
+    font-family: 'Noto Sans KR', sans-serif;
+  } 
+  select,
+  input, 
+  button,
+  textarea {
+    border: 0;
+    outline: 0 !important;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
