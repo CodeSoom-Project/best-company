c46bf26d129a248ef163db0c719812eefdda65b9
minsuk <alstjr0183@naver.com>
2022-09-26 19:33:06 +0900

    feat: flexbox 모듈 추가

---
+++ src/styles/utils/flexbox.ts
@@ -0,0 +1,23 @@
+import { css } from "styled-components";
+
+function optionAbbreviation(value: string) {
+  const flexs: {
+    [index: string]: string;
+  } = {
+    start: "flex-start",
+    end: "flex-end",
+    between: "space-between",
+    around: "space-around",
+    value,
+  };
+
+  return flexs[value];
+}
+
+export function flexbox(jc = "center", ai = "center") {
+  return css`
+    display: flex;
+    justify-content: ${optionAbbreviation(jc)};
+    align-items: ${optionAbbreviation(ai)};
+  `;
+}
